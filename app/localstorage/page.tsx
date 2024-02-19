b79775becb830d6e8ed128d518e7d7eff22df409
21ftt1750 <99001373+21ftt1750@users.noreply.github.com>
2024-02-19 16:35:57 +0800

    function saving listitems to localstorage working

--- app/localstorage/page.tsx
+++ app/localstorage/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import background from "../../public/images/project.png";
+import Link from "next/link";
+import { ChevronLeft, Pen, Trash } from "lucide-react";
+import logo from "../../public/images/logo.png";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const Page = () => {
   const [listItems, setListItems] = useState<
@@ -58,35 +71,101 @@ const Page = () => {
 
   return (
     <>
-      <div>
-        <button onClick={() => setIsDialogVisible(true)}>Add Item</button>
-      </div>
-
-      <div>
-        {filteredItems.map((item, index) => (
-          <div key={index}>{item.text}</div>
-        ))}
-      </div>
-
-      {isDialogVisible && (
-        <div>
-          <input
-            type="text"
-            placeholder="Enter new item"
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              handleAddListItem(newItem);
-              setIsDialogVisible(false);
-              setNewItem("");
-            }}
+      <div className="relative w-full h-24 flex bg-[#00040D] text-white font-mono">
+        <div className="w-1/3 flex items-center justify-start">
+          <Link
+            href={`/homepage?user=${encodeURIComponent(user)}`}
+            className="ml-8 flex items-center"
           >
-            Add
-          </button>
+            <ChevronLeft className="size-8" />
+            <Image src={logo} alt="" className="size-16" />
+          </Link>
         </div>
-      )}
+        <div className="w-1/3 flex items-center justify-center">
+          <p className="text-2xl">{projectName}</p>
+        </div>
+        <div className="w-1/3 mr-4 flex items-center justify-end">
+          <p className="mr-2 flex items-end">
+            Hello,{" "}
+            <a href="" className=" ml-1 font-bold uppercase  ">
+              {user}
+            </a>
+            !
+          </p>
+          <Dialog>
+            <DialogTrigger className=" border rounded-md flex justify-center w-28 h-8 items-center">
+              <p className="text-sm font-mono">Sign Out</p>
+            </DialogTrigger>
+            <DialogContent className="bg-[#070019] text-white font-mono h-40 flex items-center justify-center">
+              <div>
+                <DialogTitle className="text-center mb-2">
+                  Confirm Sign Out
+                </DialogTitle>
+                <p className="text-center text-sm mb-4">
+                  Are you sure you want to Sign Out of{" "}
+                  <a href="" className=" font-bold uppercase">
+                    {user}
+                  </a>
+                  ?
+                </p>
+                <div className="w-full flex justify-center">
+                  <DialogClose
+                    onClick={() => {
+                      window.location.href = "/";
+                    }}
+                    type="button"
+                    className="bg-[#10142c]  h-10 w-16 rounded-md text-[#D298FF] text-sm mr-4"
+                  >
+                    Yes
+                  </DialogClose>
+                  <DialogClose
+                    type="button"
+                    className="bg-[#10142c]  h-10 w-16 rounded-md text-[#D298FF] text-sm"
+                  >
+                    No
+                  </DialogClose>
+                </div>
+              </div>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+
+      <div className="relative bg-[#f5f5f5] h-96">
+        <Image src={background} alt={"background"} />
+
+        <div className="absolute top-0 left-0 ml-96 mt-32 text-white flex flex-col z-10 ">
+          <div className="custom-input-container flex flex-col text-black">
+            <button onClick={() => setIsDialogVisible(true)}>Add Item</button>
+
+            <div>
+              {filteredItems.map((item, index) => (
+                <div key={index}>{item.text}</div>
+              ))}
+            </div>
+
+            {isDialogVisible && (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Enter new item"
+                  value={newItem}
+                  onChange={(e) => setNewItem(e.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    handleAddListItem(newItem);
+                    setIsDialogVisible(false);
+                    setNewItem("");
+                  }}
+                >
+                  Add
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
